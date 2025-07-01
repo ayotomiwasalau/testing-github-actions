0fa5a1aba89737d5e862c1ae7a1e0c28f52c65dc
ayotomiwasalau <ayotomiwasalau@gmail.com>
2025-07-01 16:22:13 +0100

    push blue green

--- new_bucket.tf
+++ new_bucket.tf
@@ -5,21 +5,21 @@ provider "aws" {
 }
 
 # Create an S3 bucket
-resource "aws_s3_bucket" "udabucket-tomm" {
-  bucket = "cicd-terraform-demo-bucket20213-tommy"
-
-  tags = {
-    Name        = "CICD test bucket"
-    Environment = "Dev"
-  }
-}
+#resource "aws_s3_bucket" "udabucket-tomm" {
+#  bucket = "cicd-terraform-demo-bucket20213-tommy"
+#
+#  tags = {
+#    Name        = "CICD test bucket"
+#    Environment = "Dev"
+#  }
+#}
 
 # Create an EC2 instance
-resource "aws_instance" "ec2_instance" {
-  ami           = "ami-04a0ae173da5807d3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "CICD test instance"
-  }
-}
+#resource "aws_instance" "ec2_instance" {
+#  ami           = "ami-04a0ae173da5807d3"
+#  instance_type = "t2.micro"
+#
+#  tags = {
+#    Name = "CICD test instance"
+#  }
+#}
